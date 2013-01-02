c084b81129b9a60cc3368a1dd7055b1d1a930dbe
procepitation <procepitation@gmail.com>
2013-01-02 20:35:25 +0100

    Parsing scratch setup, now to fill in all the flowElement stuff..

--- src/parserScratch.js
+++ src/parserScratch.js
@@ -7,7 +7,6 @@
  * 3. Weergeven om het resultaat in een opmerking na de selectie in te voeren. (Ctrl+L)
  */
 
-console.log( "");
 console.log( "----------------" );
 
 // var bpmn2File = "MultiThreadServiceProcess.bpmn2";
@@ -35,112 +34,196 @@ if (window.DOMParser) {
   xmlDoc.loadXML(bpmn2Txt); 
 }
 
-// -=------------------
+// =====
+// BASE TYPES
+// =====
 
 function Perror(msg) { 
     this.message = msg;
 }
 Perror.prototype = new Error();
 
-var showDeep = function(lvl, node) { 
+// ----
+// COMPILATION
+// ----
 
-  var ind = "";
-  for( var i = 0; i < lvl; ++i ) { 
-    ind += "  ";
-  }
+var compile = function(node) { 
 
-  if( node.nodeType == 1 ) {
-      if( node.localName != "BPMNDiagram" ) { 
-        console.log( ind + "n: " + node.localName );
-      } else { 
-        return;
+  var schema = new Object();
+  var bpmn2Prefix = null;
+  var bpmn2SchemaRE = new RegExp( "http://www.omg.org/spec/BPMN/201\\d+/MODEL" );
+
+  // ---
+  // Functions
+  // ---
+  var definition = function(node) {
+    var nodeName = "definitions";
+    for( var i = 0; i < node.childNodes.length; ++i ) { 
+      var child = node.childNodes[i];
+      if( child.nodeType == 1 && child.localName == nodeName ) { 
+        node = child;
       }
-  } else if( node.nodeType == 3 ) { 
-    if( /[^\s]/.test(node.data) ) { 
-      console.log( ind + "n: " + node.nodeName + " [" + node.data + "]" );
     }
-  }  else { 
-      console.log( ind + "n: " + node.localName + " [" + node.nodeType + "]" );
-  }
-  
-  ind += "a";
-  if( node.attributes !== undefined && node.attributes != null ) { 
+    if( node.localName != nodeName ) { 
+      throw new Perror( "element local name is " + node.localName + "; expected '" + nodeName + "'" );
+    } 
     for( var i = 0; i < node.attributes.length; ++i ) { 
       var attr = node.attributes[i];
-      console.log( ind + "[" + i + "] (" + attr.prefix + ") " + attr.localName + " : " + attr.value );
-    } 
+      if( /^xmlns$/i.test( attr.prefix ) ) { 
+        schema[attr.localName] = attr.value;
+        if( bpmn2SchemaRE.test(attr.value) ) { 
+          bpmn2Prefix = attr.localName;
+        }  
+      } 
+    }
+    return node;
+  }
+
+    var processNode = { 
+      adHocSubProcess: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      boundaryEvent: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      businessRuleTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      callActivity: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      callChoreography: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      choreographyTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      complexGateway: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      dataObject: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      dataObjectReference: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      dataStoreReference: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      endEvent: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      event: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      eventBasedGateway: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      exclusiveGateway: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      implicitThrowEvent: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      inclusiveGateway: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      intermediateCatchEvent: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      intermediateThrowEvent: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      manualTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      parallelGateway: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      receiveTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      scriptTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      sendTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      sequenceFlow: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      serviceTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      startEvent: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      subChoreography: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      subProcess: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      task: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      transaction: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+      userTask: function(node) { 
+        throw new Perror(node.localName + " is not yet supported.");
+      },
+    };
+
+  var process = function(node) { 
+    for( var i = 0; i < node.childNodes.length; ++i ) { 
+      if( node.childNodes[i].prefix == bpmn2Prefix && node.childNodes[i].nodeType == 1 ) { 
+        var child = node.childNodes[i];     
+        console.log( child.localName );
+        processNode[child.localName](child);    
+      }
+    }
   }
   
-  if( node.childNodes !== undefined ) { 
-    for( var i = 0; i <  node.childNodes.length; ++i ) { 
-      showDeep( lvl + 1, node.childNodes[i] )
+  // ---
+  // Compilation
+  // ---
+  var defNode = definition(node);
+
+  for( var i = 0; i < defNode.childNodes.length; ++i ) {
+    var child = defNode.childNodes[i];
+    if( child.prefix == bpmn2Prefix && child.nodeType == 1 ) { 
+      switch(child.localName) { 
+        case "process":
+          var procNode = process(child);
+          break;
+        case "itemDefinition":
+          break;
+        case "error":
+          break;
+        case "signal":
+          break;
+        case "dataStore":
+          break;
+        case "escalation":
+          break;
+        case "message":
+          break;
+        case "resource":
+          break;
+        case "interface":
+          break;
+        default: 
+          throw new Perror("Root-level '" + child.localName + "' elements are not supported.");
+      }
     }
   }
-}
-
-// showDeep( 0, xmlDoc );
-
-var schema = new Object();
-var bpmn2Prefix = null;
-var bpmn2SchemaRE = new RegExp( "http://www.omg.org/spec/BPMN/201\\d+/MODEL" );
-
-var compile = function( node ) { 
-    for( var i = 0; i <  node.childNodes.length; ++i ) { 
-       var child = node.childNodes[i];
-       console.log( "n: (" + child.prefix + ") " + child.localName + " [" + child.nodeType + "]" );
-       for( var i = 0; i < child.attributes.length; ++i ) { 
-         var attr = child.attributes[i];
-         console.log( "[" + i + "] (" + attr.prefix + ") " + attr.localName + " : " + attr.value );
-       } 
-    }
-    return null;
-}
-
-
-var definition = function( node ) { 
-  if( node.localName != "definition" ) { 
-    throw new Perror( "element local name is " + node.localName + "; expected 'definition'" );
-  } 
-  for( var i = 0; i < child.attributes.length; ++i ) { 
-    var attr = child.attributes[i];
-    if( /^xmlns$/i.test( attr.localName ) ) { 
-      schema[attr.prefix] = attr.value;
-      if( bpmn2SchemaRE.test(attr.value) ) { 
-        bpmn2Prefix = attr.prefix;
-      }  
-    }    
-  }  
+  
+  // return process instance (array)
+  return null;  
 }
 
 var instance = compile( xmlDoc );
 
-// -----------
-
-
-var nodeAccept = [
-  NodeFilter.FILTER_REJECT, // NO type 0!!
-  NodeFilter.FILTER_ACCEPT, // 1: element 
-  NodeFilter.FILTER_ACCEPT, // attribute
-  NodeFilter.FILTER_ACCEPT, // text
-  NodeFilter.FILTER_ACCEPT, // CDATA 
-  NodeFilter.FILTER_ACCEPT, // 5: entity reference 
-  NodeFilter.FILTER_ACCEPT, // entity 
-  NodeFilter.FILTER_REJECT, // processing instruction 
-  NodeFilter.FILTER_ACCEPT, // element 
-  NodeFilter.FILTER_ACCEPT, // 9: document 
-  NodeFilter.FILTER_ACCEPT, // document type
-  NodeFilter.FILTER_ACCEPT, // document fragment
-  NodeFilter.FILTER_ACCEPT, // notation
-];
-  
-var nodeFilter = function(node) { 
-  if( node.nodeType == 1 && node.localName == "BPMNDiagram" ) { 
-    return NodeFilter.FILTER_REJECT;
-  }
-  return nodeAccept[node.nodeType];
-}
-
-/*
-Exception: perror is not defined
-@Scratchpad:43
-*/
+console.log("===");
+console.log("");
