9d5be68ab821e16f65bad994fed9e5f7331b8559
procepitation <procepitation@gmail.com>
2013-01-06 13:57:49 +0100

    Update to tests

--- test/unit/compiler-createRep-tests.js
+++ test/unit/compiler-createRep-tests.js
@@ -14,5 +14,7 @@ test( "MinimalProcess compilation test", function() {
   ok( procRep != null, "non-null process representation" );
   ok( procRep.seq.length == 2, procRep.seq.length + " ? " + 2 + " seqence")
   var l = 0; for( i in procRep.step ) { ++l; } 
-  ok( l == 3, l + " ? " + 3 + " steps")
+  ok( l == 3, l + " ? " + 3 + " steps");
+  var e = "terminateEventDefinition"
+  ok( procRep.step["_3"][e], e );
 });
