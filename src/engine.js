c9ab7f3652a827c785f68d187a2ebd570573d880
procepitation <procepitation@gmail.com>
2013-07-29 10:56:41 +0200

    Small fixes and some work on the engine

--- src/engine.js
+++ src/engine.js
@@ -5,26 +5,47 @@ if( typeof runme == 'undefined' ) {
 
 runme.runProcess = function(process, defInst) { 
 
-  function Context() { 
+  function Context(execBranchFunction) { 
+    this.execBranch = execBranchFunction;
     this.log = function(msg) { 
       runme.log(msg);
     }
   }
 
+  var executeSequence = funciont(inst) { 
+    var inst = process.inst;
+    for( var a = 0; a < inst.length; ++a ) { 
+      // * context pre-processing: 
+      // ??
+  
+      inst[a].execute(context);
+  
+      // * context post-processing: 
+      // ??
+  
+      // persistence? 
+    }
+
+    return inst[inst.length-1];
+  }
+
+  var executeBranch = function(node, context) { 
+    node.execute(context);
+  }
+
   // TODO: create a context specific object? 
-  var context = new Context();
+  var context = new Context(this.executeBranch);
 
-  var inst = process.inst;
-  for( var i = 0; i < inst.length; ++i ) { 
-    // * context pre-processing: 
-    // ??
-
-    inst[i].execute(context);
-
-    // * context post-processing: 
-    // ??
-
-    // persistence? 
+  var branches = [];
+  branches.push(process.inst);
+  while( branches.length > 0 ) { 
+    var branchSeq = branches.shift();
+    var endBranch = executeSequence(branchSeq);
+    if( endBranch != null ) { 
+      for( b = endBranch.branches.length-1; b >= 0; --b ) { 
+        branches.push( endBranch.branches[b] );
+      }
+    }
   }
 
 };
